8705de3540880bb55128e7f56d2f93bdc209a21c
Sissoko <m.sissoko7035@gmail.com>
2023-09-14 11:28:45 +0000

    Initialisation du project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  searchTitle='';
+  movieUrl='';
+movieData={
 
-  constructor() {}
+  title:'',
+  imageUrl:'',
+  description:''
+}
+descriptionv2:'';
 
+  constructor(
+    private http:HttpClient,
+
+    ) {
+
+    }
+    ngOnInit(){
+     
+    }
+
+
+    readApi(URL:string){
+     
+      return this.http.get(URL)
+     
+      
+    }
+  searchMovie(){
+
+    console.log("recherche de film "+this.searchTitle);
+    const search =encodeURIComponent(this.searchTitle).trim();
+    console.log(search);
+    this.movieUrl="https://www.omdbapi.com/?apikey=851dde8f&t=" + search
+    this.readApi(this.movieUrl)
+.subscribe((data)=>{
+  console.log('Data : ', data)
+  this.movieData.title =data['Title'];
+  this.movieData.description=data['Plot'];
+  this.movieData.imageUrl=data['Poster']
+  console.log(`Movie Title ${this.movieData.title}`);
+
+})
+  }
 }
