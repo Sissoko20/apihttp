33ce3f13a6c4e58bb5d1bf2571863d3f544e11b8
Sissoko <m.sissoko7035@gmail.com>
2023-09-14 13:49:05 +0000

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularFireModule } from "@angular/fire/compat";
+
 
 
 
@@ -15,7 +15,7 @@ import { AngularFireModule } from "@angular/fire/compat";
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
-    AngularFireModule
+
   
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }],
